66f84f566c68a232a8e94f01e9ba85ffa43bfb93
anishkarki <anish.karki3579@gmail.com>
2018-11-04 17:06:47 +0545

    this

---
+++ routes/about.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET about page. */
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+module.exports = router;

aa8831182d9496d063576ea8120fa12e445980bc
anishkarki <anish.karki3579@gmail.com>
2018-11-04 17:11:06 +0545

    New comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
-
+//this is check
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
